0b404ac45c9f108c249eb7ea456f95809bae28fe
Bradley_Carson <bmcarson1671@gmail.com>
2017-08-04 13:51:38 -0400

    added win/loss and counter

--- js/main.js
+++ js/main.js
@@ -22,17 +22,36 @@ let alphabet = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 
 //array to store letters you have guessed when pressing alphabet button
 let lettersGuessed = [];
+
+//creates the answer area
+wordLetter.forEach(function(item, index){
+  let button = document.createElement('p');
+  button.textContent = '_';
+  wordRow.appendChild(button);
+})
+//creates the row of guessed letters
+let guessed = document.createElement('p');
+guessedRow.appendChild(guessed);
+
+//selects area to put win or loss message
+let winOrLossArea = document.querySelector('.win-or-loss');
+//attempts counter
+let attemptCounter = document.createElement('p');
+let attempts = wordLetter.length + 5;
+attemptCounter.textContent = 'Attempts Remaining: ' + attempts;
+winOrLossArea.appendChild(attemptCounter);
+
 //creates the alphabet buttons, adds event listener, runs funtions, then pushes content to screen
 alphabet.forEach(function(item, index){
   let button = document.createElement('button');
   button.textContent = item;
   button.addEventListener('click', function(e){
     //e.target selects the target of the event listener e (e is the thing triggering the event)
-    var button = e.target;
+    let button = e.target;
     //functions that run on button click
     recordGuess(button);
     checkGuess(button);
-    displayGuessed(button);
+    winGame();
   });
   buttonRow.appendChild(button);
 });
@@ -49,38 +68,47 @@ function checkGuess(button){
     if (guess === letter) {
       displayLetters[index].textContent = letter;
     }
-  })
-
+  });
+  console.log('check guess');
 }
 //function that records the guess in the array
 function recordGuess(button){
+  // if a letter has already been guessed, bail
   if (lettersGuessed.includes(button.textContent)) {
     return
-  } else {
-    lettersGuessed.push(button.textContent);
-    return console.log(lettersGuessed);
   }
+  // record the guessed letter and display on screen
+  lettersGuessed.push(button.textContent);
+  guessed.textContent = lettersGuessed;
+  attempts = attempts - 1;
+  //if attempts is 0, bail so numbers don't go negative
+  if (attempts <= -1){
+    return
+  }
+  attemptCounter.textContent = 'Attempts Remaining: ' + attempts;
 }
-//adds guessed letters to guessed row once
-function displayGuessed(button){
-  guessed.textContent = 'Letters Guessed: ' + lettersGuessed;
+
+
+//creates and sets win message to empty string
+let winMessage = document.createElement('p');
+winMessage.textContent = '';
+winOrLossArea.appendChild(winMessage);
+//displays winning message
+function winGame(){
+  //array.prototype.slice.call(whatever) will turn a nodelist into an array
+  let displayLetters = Array.prototype.slice.call(document.querySelectorAll('.words p'));
+  //get text content from above array and change array to letters
+  let displayLettersArray = displayLetters.map(function(item){
+    return item.textContent;
+  });
+  //if the letters on screen match the letters of the word, display win message
+  if (displayLettersArray.join() === wordLetter.join() && attempts > 0) {
+    winMessage.textContent = 'You Win!';
+  } else if (attempts === 0) {
+    winMessage.textContent = 'Loser! You\'re a loser! You big baby!';
+  }
 };
-
-
-//creates the answer area
-wordLetter.forEach(function(item, index){
-  let button = document.createElement('p');
-  button.textContent = '_';
-  wordRow.appendChild(button);
-})
-//creates the row of guessed letters
-let guessed = document.createElement('p');
-guessedRow.appendChild(guessed);
-
-
-
-
-
+//displays losing message
 
 
 }());
